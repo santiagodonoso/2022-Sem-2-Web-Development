3b47e7454e81d80a89b62e9d36f262b21e8ac4b7
Santiago Donoso <sand@kea.dk>
2022-08-24 16:34:21 +0200

    ...

---
+++ 23-Aug-2022/cities.php
@@ -0,0 +1,10 @@
+<ul>
+<?php
+  // Array of cities
+  $cities = ['Copenhagen', 'Miami', 'Barcelona'];
+  // Loop through the cities : for loop and for each loop
+  foreach( $cities as $city){
+    echo "<li>$city</li>";
+  }
+?>
+</ul>
