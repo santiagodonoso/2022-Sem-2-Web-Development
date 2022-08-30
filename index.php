a2488f78dc0a2695a99755c3ac2217effe8efefa
Santiago Donoso <sand@kea.dk>
2022-08-30 10:05:27 +0200

    ...

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <header>  
+    <div>
+      <img src="logo.png" alt="momondo.png">
+    </div>
+    <div>
+      <a href="trips">Trips</a>
+      <a href="login">Login</a>
+      <a href="danish">&#127988; Dansk</a>
+    </div>
+  </header>
+
+  <div id="flights-search">
+
+  </div>
+
+  <div id="main">
+    <div id="left">
+      left
+    </div>
+    <div id="right">
+      right
+    </div>
+  </div>
+
+  <footer>
+    footer
+  </footer>
+
+</body>
+</html>
