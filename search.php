29bdd3e09cefa833fc086812f46f32237b59fae1
Santiago Donoso <sand@kea.dk>
2022-09-01 10:11:42 +0200

    ...

---
+++ search.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="app.css">
+</head>
+<body>
+  <form onsubmit="return false">
+    <button>
+      Search
+    </button>
+  </form>
+
+  <div id="flights">
+
+    <div class="flight">
+
+    </div>
+
+  </div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
