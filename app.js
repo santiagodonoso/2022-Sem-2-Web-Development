6e8b12f7c3b7377e90fb98091314b9428aff8302
Santiago Donoso <sand@kea.dk>
2022-09-01 10:30:59 +0200

    ...

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 async function get_flights(){
-  const conn = await fetch('api-search.php')
+  const conn = await fetch('api-search.php?from_city=a')
   const flights = await conn.json()
   console.log(flights)
+  flights.forEach( flight => {
+    console.log(flight)
+  } )
 }
