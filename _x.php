97b7e8094a752f00838a387a3575d6635624e9ac
Santiago Donoso <sand@kea.dk>
2022-09-06 09:14:53 +0200

    ...

--- _x.php
+++ _x.php
@@ -6,15 +6,15 @@ define('_ITEM_NAME_MAX_LEN', 10);
 function _validate_item_name(){
   $error_message = 'item_name min 2 max 10 characters';
   if( ! isset($_POST['item_name']) ){
-    _respond();
+    _respond(500);
   }
 }
 
 // ##############################
-function _respond(){
+function _respond( $message = '',  $http_response_code = 200 ){
   header('Content-Type: application/json');
-  http_response_code(400);
-  $response = ['info'=>'upsss...'];
+  http_response_code($http_response_code);
+  $response = ['info'=>$message];
   echo json_encode($response);
   exit();
 }
