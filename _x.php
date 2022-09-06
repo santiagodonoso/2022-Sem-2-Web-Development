da35cbacf340a509b037fa7cdc8c6e7f1dc46d8f
Santiago Donoso <sand@kea.dk>
2022-09-06 10:12:10 +0200

    ...

--- _x.php
+++ _x.php
@@ -4,6 +4,10 @@ define('_ITEM_NAME_MAX_LEN', 10);
 
 define('_USER_NAME_MIN_LEN', 2);
 define('_USER_NAME_MAX_LEN', 20);
+define('_USER_LAST_NAME_MIN_LEN', 2);
+define('_USER_LAST_NAME_MAX_LEN', 20);
+
+define('_REGEX_EMAIL', '/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/');
 
 // ##############################
 function _validate_item_name(){
@@ -23,6 +27,16 @@ function _validate_user_name(){
   if( strlen($_POST['user_name']) < _USER_NAME_MIN_LEN ){ _respond($error_message, 400); }   
   if( strlen($_POST['user_name']) > _USER_NAME_MAX_LEN ){ _respond($error_message, 400); }   
   return $_POST['user_name'];
+}
+
+// ##############################
+function _validate_user_last_name(){
+  $error_message = 'user_last_name min '._USER_LAST_NAME_MIN_LEN.' max '._USER_LAST_NAME_MAX_LEN.' characters';
+  if( ! isset($_POST['user_last_name']) ){ _respond($error_message, 400); }
+  $_POST['user_last_name'] = trim($_POST['user_last_name']);
+  if( strlen($_POST['user_last_name']) < _USER_LAST_NAME_MIN_LEN ){ _respond($error_message, 400); }   
+  if( strlen($_POST['user_last_name']) > _USER_LAST_NAME_MAX_LEN ){ _respond($error_message, 400); }   
+  return $_POST['user_last_name'];
 }
 
 // ##############################
--- api-signup.php
+++ api-signup.php
@@ -2,6 +2,7 @@
 require_once __DIR__.'/_x.php';
 
 _validate_user_name();
+_validate_user_last_name();
 
 // Success
 _respond('user created');
