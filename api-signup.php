4c76d524ea6fdc4648b16384163c33ef1c8b213a
Santiago Donoso <sand@kea.dk>
2022-09-06 10:28:41 +0200

    ...

--- api-signup.php
+++ api-signup.php
@@ -5,8 +5,15 @@ _validate_user_name();
 _validate_user_last_name();
 _validate_email();
 
+$user = [
+  'user_id' => uniqid(),
+  'user_name' => $_POST['user_name'],
+  'user_last_name' => $_POST['user_last_name'],
+  'user_email' => $_POST['email']
+];
+
 // Success
-_respond('user created');
+_respond($user);
 
 
 
