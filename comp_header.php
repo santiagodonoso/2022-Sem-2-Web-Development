0702f702f969ffb1b97c893db4b2ce0a4305dbf0
Santiago Donoso <sand@kea.dk>
2022-09-13 11:26:38 +0200

    ...

--- comp_header.php
+++ comp_header.php
@@ -5,7 +5,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="app.css">
-  <title>Document</title>
+  <title>
+    <?= $_title ?? 'Momondo' ?>
+  </title>
 </head>
 <body oncontextmenu="toogle_menu(); return false">  
 
@@ -20,7 +22,4 @@
     <button onclick="toogle_menu()">☰</button>
   </nav>
 
-  <script src="app.js"></script>
-
-</body>
-</html>
+  
---
+++ view_index.php
@@ -0,0 +1,20 @@
+<?php
+$_title = 'Momondo';
+require_once __DIR__.'/comp_header.php';
+?>
+
+<main>
+  Index
+  <form>
+    <input type="text" placeholder="from">
+    <input type="text" placeholder="to">
+    <button>Search</button>
+  </form>
+</main>
+
+
+<?php
+require_once __DIR__.'/comp_footer.php';
+?>
+
+
